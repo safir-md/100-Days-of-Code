37481078e9925c2003f66e0781f5236bc7a1eace
Safir Mohammad Mustak Shaikh <safirmd47@gmail.com>
2023-05-14 15:44:26 +0200

    Update maze.py

--- Reeborg's World/maze.py
+++ Reeborg's World/maze.py
@@ -3,9 +3,10 @@ def turn_right():
     turn_left()
     turn_left()
 
-while(not is_facing_north()):
-    turn_left()
-    
+while(front_is_clear()):
+    move()
+turn_left()
+
 while(not at_goal()):
     if(right_is_clear()):
         turn_right()
