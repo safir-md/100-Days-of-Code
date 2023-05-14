c1b4e5f314149bb87e4a08e3de396aa2ec4c8cf6
Safir Mohammad Mustak Shaikh <safirmd47@gmail.com>
2023-05-14 09:53:34 +0200

    Update main.py File

--- Rock-Paper-Scissors/main.py
+++ Rock-Paper-Scissors/main.py
@@ -29,15 +29,23 @@ scissors = '''
 import random
 game = [rock, paper, scissors]
 
+def display(number, rand_num):
+  print("\nYou chose:\n",game[number])
+  print("\nComputer chose:\n",game[rand_num])
+
 while(True):
   number = int(input("\nEnter 0 for Rock, 1 for Paper, or 2 for Scissors: "))
-  print("\nYou chose:\n",game[number])
   rand_num = random.randint(0,2)
-  print("\nComputer chose:\n",game[rand_num])
-  if((number == 2 and rand_num == 0) or (rand_num > number) and not(rand_num == 2 and number == 0)):
+  if(number>2 or number<0):
+    print("Wrong Number, You lose!")
+    break
+  elif((number == 2 and rand_num == 0) or (rand_num > number) and not(rand_num == 2 and number == 0)):
+    display(number, rand_num)
     print("\nYou lose")
     break
   elif(rand_num == number):
+    display(number, rand_num)
     print("\nDraw")
   else:
+    display(number, rand_num)
     print("\nYou win")
